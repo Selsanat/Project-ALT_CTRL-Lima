ed511b9ee1d4892853b23e49e89745245ccbb8eb
Melvin <74654895+MichenaudMelvin@users.noreply.github.com>
2024-10-21 10:10:58 +0200

    [ADD] new characters sprite + every character has a prefab

--- Project-ALT_CTRL-Lima/Assets/Scripts/Dialogues/DialogsController.cs
+++ Project-ALT_CTRL-Lima/Assets/Scripts/Dialogues/DialogsController.cs
@@ -124,7 +124,7 @@ public class DialogsController : MonoBehaviour
         {
             command.Value.ForEach(c =>
             {
-                print(command.Key);
+                //print(command.Key);
                     c.Init(_dialogText, command.Key);
             });
         }
